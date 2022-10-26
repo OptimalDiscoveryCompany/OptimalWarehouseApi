8f8a0680222290d5183939c07f7e9cbe80c90f97
Gabriel Bernardo <gabriel7798@gmail.com>
2022-10-26 12:22:15 -0300

    finish backend and start fron end

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Core.Entities
+namespace Core.Entities;
+
+public class BaseEntity
 {
-    public class BaseEntity
-    {
-        public int Id { get; set; }
-    }
+    public int Id { get; set; }
 }
